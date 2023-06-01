482174122aefc8a3b563ffd1bb77c1451ec0ad9e
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-05-31 20:46:35 -0500

    completed the mobile menu

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ function opentab(tabname) {
       document.getElementById(tabname).classList.add('active-tab')
 }
 
-// Resume Button
-const openResume = () => {
-      window.open('https://docs.google.com/document/d/1iCUHBRbxb4lBuxGkZ1mORutcVE1NqPiatgZI2MlL0V0/edit?usp=sharing', '_blank');
-    }
+// Side Menu
+let sidemenu = document.getElementById('sidemenu')
+function openmenu() {
+      sidemenu.style.right = '0'; 
+}
+function closemenu() {
+      sidemenu.style.right = '-200px'; 
+}
